b67876b9f0b634ef962ce783b3a023c7334d8e08
emma <104070share@gmail.com>
2024-07-17 12:17:27 +0800

    更改 imageBox

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -136,19 +136,6 @@ export default defineConfig({
             {
               src: "https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js",
             },
-        ],
-        [
-            "script",
-            {},
-            `
-              const script = document.createElement("script");
-              script.src = "https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js";
-              document.body.append(script);
-              const link = document.createElement("link");
-              link.rel = "stylesheet";
-              link.href = "https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.css";
-              document.head.append(link);
-            `,
-          ],
+        ]
     ]
 })
