0abd5aa55319405f257f2a2d1a6b1e664aa2049d
mac <mac@104070.com>
2024-07-17 15:55:06 +0800

    修复tool/sidebar

--- docs/.vitepress/sideBar.ts
+++ docs/.vitepress/sideBar.ts
@@ -1,12 +1,10 @@
 import {DefaultTheme} from "vitepress";
 
 import toolsSideBar from "../tools/toolsSideBar";
-import dockerSideBar from "../docker/dockerSideBar";
 
 
 const sideBar: DefaultTheme.SidebarMulti = {
     '/tools/': toolsSideBar,
-    '/docker/': dockerSideBar
 }
 
 
