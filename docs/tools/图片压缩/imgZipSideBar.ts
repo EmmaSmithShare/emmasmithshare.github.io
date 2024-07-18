88f53d1d1d983d32348200f1b454a4b027baf803
emma <104070share@gmail.com>
2024-07-18 20:53:44 +0800

    首次提交

---
+++ docs/tools/图片压缩/imgZipSideBar.ts
@@ -0,0 +1,11 @@
+import {DefaultTheme} from "vitepress";
+
+const imgZipSideBar: DefaultTheme.SidebarItem = {
+    text: '图片视频压缩',
+    items: [
+        {text: 'Pixzip', link: '/tools/图片压缩/pixzip'},
+        {text: '鸭力巨大', link: '/tools/图片压缩/鸭力巨大'},
+    ]
+}
+
+export default imgZipSideBar
---
+++ docs/tools/文字卡片/textCardSideBar.ts
@@ -0,0 +1,11 @@
+import {DefaultTheme} from "vitepress";
+
+const textCardSideBar: DefaultTheme.SidebarItem = {
+    text: '文字卡片',
+    items: [
+        {text: 'Memo Card', link: '/tools/文字卡片/MemoCard'},
+        {text: '流光卡片', link: '/tools/文字卡片/流光卡片'},
+    ]
+}
+
+export default textCardSideBar
