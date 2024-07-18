78e4e188ffe7443afa2d570c78b951c62b27f25e
emma <104070share@gmail.com>
2024-07-18 20:54:01 +0800

    代码重构

--- docs/tools/toolsSideBar.ts
+++ docs/tools/toolsSideBar.ts
@@ -1,14 +1,18 @@
 import {DefaultTheme} from "vitepress";
+import imgZipSideBar from "./图片压缩/imgZipSideBar";
+import textCardSideBar from "./文字卡片/textCardSideBar";
 
 
 const toolsSideBar: DefaultTheme.SidebarItem[] = [
     {text: '筛选', link: '/tools/index'},
+    textCardSideBar,
+    imgZipSideBar,
     {
         text: '孤品',
         items: [
-            { text:'emoji查询', link:'/tools/孤品/emoji'}
+            {text: 'emoji查询', link: '/tools/孤品/emoji'}
         ]
-    }
+    },
 ]
 
 
