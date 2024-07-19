b8da76f89dd96bada05504fa7d10758f23a1e9e6
emma <104070share@gmail.com>
2024-07-19 10:00:09 +0800

    更改图片类型为 png, svg图片 twitter不显示

--- docs/.vitepress/theme/twitterCard.ts
+++ docs/.vitepress/theme/twitterCard.ts
@@ -9,11 +9,11 @@ const twitterCard = (pageData: PageData, ctx: TransformPageContext) => {
     // 构造每页的 Header
     const ogTitle: HeadConfig = ["meta", {property: "og:title", content: frontmatter.title}]
     const ogDescription: HeadConfig = ["meta", {property: "og:description", content: frontmatter.description || siteDescription}]
-    const ogImage: HeadConfig = ["meta", {property: "og:image", content: frontmatter.image || 'https://www.104070.xyz/logo/logo_light.svg'}]
+    const ogImage: HeadConfig = ["meta", {property: "og:image", content: frontmatter.image || 'https://www.104070.xyz/logo/logo_png.png'}]
 
     const twitterCard: HeadConfig = ["meta", {name: 'twitter:card', content: 'summary'}]
     const twitterSite: HeadConfig = ["meta", {content: '@104070Share'}]
-    const twitterImage: HeadConfig = ["meta", {name: "twitter:image:src", content: frontmatter.image || 'https://www.104070.xyz/logo/logo_light.svg'}]
+    const twitterImage: HeadConfig = ["meta", {name: "twitter:image:src", content: frontmatter.image || 'https://www.104070.xyz/logo/logo_png.png'}]
     const twitterDescription: HeadConfig = ["meta", {name: "twitter:description", content: frontmatter.description || siteDescription}]
 
 
