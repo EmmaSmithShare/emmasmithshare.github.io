8dcb770b05bc52a6d71db8b22ecd5f8ddcfd3a50
emma <emma@emma.com>
2024-08-06 23:58:10 +0800

    更新头像

--- docs/tools/toolsSideBar.ts
+++ docs/tools/toolsSideBar.ts
@@ -3,6 +3,7 @@ import imgZipSideBar from "./图片压缩/imgZipSideBar";
 import imgCupSidebar from "./截图/imgCupSidebar";
 import textCardSideBar from "./文字卡片/textCardSideBar";
 import colorSideBar from "./调色板/sideBar";
+import avatarSidebar from "./头像/avatarSidebar";
 
 
 const toolsSideBar: DefaultTheme.SidebarItem[] = [
@@ -11,6 +12,7 @@ const toolsSideBar: DefaultTheme.SidebarItem[] = [
     imgZipSideBar,
     imgCupSidebar,
     colorSideBar,
+    avatarSidebar,
     {
         text: '孤品',
         collapsed: true,
